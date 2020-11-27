a874ced951849e55f13c41b7839072693840a037
Titouan <>
2020-11-27 23:09:07 +0000

    Created App on Xcode.

---
+++ Front End/Bestagram/Bestagram/BestagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestagramApp.swift
+//  Bestagram
+//
+//  Created by Titouan Blossier on 27/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BestagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
