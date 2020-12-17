17f49ae58558b529a8cabfa890167de3e366c47f
Titouan <>
2020-12-17 18:16:36 +0000

    Trying another approach using a utility to generate certs.

--- Back End/main.py
+++ Back End/main.py
@@ -12,4 +12,4 @@ api = Api(app)
 api.add_resource(login.Login, "/login")
 
 # Running the api.
-app.run(host=HOST, port=PORT, ssl_context=("ApiCertificate/0.0.0.0:5002.crt", "ApiCertificate/0.0.0.0:5002.key"))
+app.run(host=HOST, port=PORT, ssl_context=("ApiCertificate/cert.pem", "ApiCertificate/key.pem"))
